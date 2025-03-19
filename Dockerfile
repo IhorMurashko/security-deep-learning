ab77bfb48f4352b566ca3cbef0f49166e462255c
Ihor Murashko <i.murashko0911@gmail.com>
2025-03-19 16:26:27 +0100

    added kit-kat controller

--- Dockerfile
+++ Dockerfile
@@ -15,17 +15,3 @@ COPY --from=builder /app/target/*.jar app.jar
 ENV SPRING_PROFILES_ACTIVE=prod
 EXPOSE 8080
 ENTRYPOINT ["java", "-jar", "app.jar"]
-
-
-
-#FROM maven:3.8.6-amazoncorretto-17 as builder
-#WORKDIR /app
-#COPY . /app
-#RUN mvn -f /app/pom.xml clean package -Pprod -Dmaven.test.skip=true
-#
-#FROM openjdk:17-jdk-slim
-#WORKDIR /app
-#COPY --from=builder /app/target/*.jar /app/app.jar
-#ENV SPRING_PROFILES_ACTIVE=prod
-#EXPOSE 8080
-#ENTRYPOINT ["java", "-jar", "/app/app.jar"]
---
+++ src/main/java/com/deepLearning/security/controllers/MyKittikateController.java
@@ -0,0 +1,25 @@
+package com.deepLearning.security.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kit")
+@RequiredArgsConstructor
+public class MyKittikateController {
+
+    @GetMapping(value = "/-kat",
+            produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<Resource> kat() {
+     Resource image = new ClassPathResource("static/photo.jpg");
+     return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(image);
+    }
+
+
+}
